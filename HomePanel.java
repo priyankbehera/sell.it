c217301da3cb8f6361a6dceb1f72ce25679703ce
Brayden Reimann <reimann@purdue.edu>
2023-11-24 14:26:19 -0500

    Added more panels.

--- HomePanel.java
+++ HomePanel.java
@@ -1,4 +1,20 @@
 import javax.swing.*;
 
 public class HomePanel extends JPanel {
+    private final JButton myAccountButton;
+
+    public HomePanel() {
+        // Set the layout manager for this panel
+        setLayout(null);
+
+        // Create components
+        this.myAccountButton = new JButton("My Account");
+        this.myAccountButton.setBounds(500, 20, 125, 25);
+        add(this.myAccountButton);
+    }
+
+    // Allows other classes to access buttons
+    public JButton getMyAccountButton() {
+        return this.myAccountButton;
+    }
 }
