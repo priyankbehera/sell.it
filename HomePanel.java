f5a9945763ce22d031772ec5e40159d19be165ce
Brayden Reimann <reimann@purdue.edu>
2023-11-24 14:39:36 -0500

    Added send message button

--- HomePanel.java
+++ HomePanel.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class HomePanel extends JPanel {
     private final JButton myAccountButton;
+    private final JButton sendMessageButton;
 
     public HomePanel() {
         // Set the layout manager for this panel
@@ -9,12 +10,20 @@ public class HomePanel extends JPanel {
 
         // Create components
         this.myAccountButton = new JButton("My Account");
-        this.myAccountButton.setBounds(500, 20, 125, 25);
+        this.myAccountButton.setBounds(450, 20, 125, 25);
         add(this.myAccountButton);
+
+        this.sendMessageButton = new JButton("Send Message");
+        this.sendMessageButton.setBounds(300, 200, 125, 25);
+        add(this.sendMessageButton);
     }
 
     // Allows other classes to access buttons
     public JButton getMyAccountButton() {
         return this.myAccountButton;
+    }
+
+    public JButton getSendMessageButton() {
+        return this.sendMessageButton;
     }
 }
