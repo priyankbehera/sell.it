b54242b682e127716b4f45342e9401cd45b4f3e7
mattallenn <104325813+mattallenn@users.noreply.github.com>
2023-11-26 19:36:01 -0500

    added method that logs in customer

--- Server.java
+++ Server.java
@@ -27,4 +27,35 @@ public class Server {
             }
         } while (true);
     }
+
+    /*
+     "logs in" the user by verifying their credentials are correct
+    returns true if user successfully logs in, false otherwise
+    accountType: 0 = customer, 1 = seller
+    */
+    public static synchronized boolean login (int accountType, String email, String password) {
+        // Sets filename based on account type
+        String filename = "";
+        if (accountType == 0) {
+            filename = "customer_data/customerNames.txt";
+        } else if (accountType == 1) {
+            filename = "seller_data/sellerNames.txt";
+        } else {
+            return false;
+        }
+        // Read from user data file and check if username and password match
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data[0].equals(email) && data[1].equals(password)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            return false;
+        }
+        return false;
+    }
 }
