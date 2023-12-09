4bacf7f29acd6f191ec867837e21c4364f8824fc
Brayden Reimann <reimann@purdue.edu>
2023-12-09 17:09:03 -0500

    Updated response for null keywords

--- Panels/MenuPanel.java
+++ Panels/MenuPanel.java
@@ -143,9 +143,15 @@ public class MenuPanel extends JPanel {
         censorKeywords.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String keyword = JOptionPane.showInputDialog(null, "Please enter a keyword:", "Censor a Keyword", JOptionPane.WARNING_MESSAGE);
-                if (setCensoredKeyword(keyword, currentUser, br, pw)) {
-                    JOptionPane.showMessageDialog(null, "Keyword " + '"' + keyword + '"' + " successfully censored.", "Censor a Keyword", JOptionPane.INFORMATION_MESSAGE);
+                boolean hasKeyword = false;
+                while (!hasKeyword) {
+                    String keyword = JOptionPane.showInputDialog(null, "Please enter a keyword:", "Censor a Keyword", JOptionPane.WARNING_MESSAGE);
+                    if (keyword.isEmpty()) {
+                        JOptionPane.showMessageDialog(null, "Error: Your keyword cannot be empty.");
+                    } else {
+                        hasKeyword = setCensoredKeyword(keyword, currentUser, br, pw);
+                        JOptionPane.showMessageDialog(null, "Keyword " + '"' + keyword + '"' + " successfully censored.", "Censor a Keyword", JOptionPane.INFORMATION_MESSAGE);
+                    }
                 }
             }
         });
@@ -190,7 +196,7 @@ public class MenuPanel extends JPanel {
                         JOptionPane.showMessageDialog(null, "Password cannot contain commas.");
                         return;
                     }
-                    String request = "editAccount," + currentUser +  "," + newUsername + "," + newPassword;
+                    String request = "editAccount," + currentUser + "," + newUsername + "," + newPassword;
                     try {
                         pw.println(request);
                         pw.flush();
@@ -294,28 +300,24 @@ public class MenuPanel extends JPanel {
     }
 
     private boolean setCensoredKeyword(String keyword, String user, BufferedReader br, PrintWriter pw) {
-        if (keyword == null) {
-            return false;
-        } else {
-            String request = "setKeyword," + user + "," + keyword;
-            boolean success = false;
-            try {
-                pw.println(request);
-                pw.flush();
+        String request = "setKeyword," + user + "," + keyword;
+        boolean success = false;
+        try {
+            pw.println(request);
+            pw.flush();
 
-                String line;
-                while ((line = br.readLine()) != null) {
-                    if (!line.isEmpty()) {
-                        break;
-                    }
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    break;
                 }
-                success = Boolean.parseBoolean(line);
-
-            } catch (IOException e) {
-                return false;
             }
-            return success;
+            success = Boolean.parseBoolean(line);
+
+        } catch (IOException e) {
+            return false;
         }
+        return success;
     }
 
     private boolean requestBlock(String blocker, String toBlock, BufferedReader br, PrintWriter pw) {
