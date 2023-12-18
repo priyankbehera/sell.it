ea03b55691ec0f732f967b019bff5297cee2a490
priyankbehera <priyankbehera1123@gmail.com>
2023-12-18 17:14:51 -0600

    database example

---
+++ Panels/DatabaseExample.java
@@ -0,0 +1,41 @@
+package Panels;
+
+import java.sql.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class DatabaseExample {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                getDatabaseInformation();
+            }
+        }, 0, 5000);
+
+    }
+
+    public static void getDatabaseInformation() {
+        String url = "jdbc:mysql://localhost:3306/giraffe";
+        String username = "root";
+        String password = "Sanupinu23";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from sellersList");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
